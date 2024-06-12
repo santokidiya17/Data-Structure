f9c5390dcd1c3280841ac07596804c2b600d9d6f
Diya Santoki <161957088+santokidiya17@users.noreply.github.com>
2024-06-12 22:13:40 +0530

    Add files via upload

---
+++ DS Lab/Lab1_5.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Lab1_5{
+    static int factorial(int n){
+    if(n==1){
+        return 1;
+    }else{
+        return n*factorial(n-1);
+    }
+}
+    public static void main(String args[]){
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter the Number:");
+       int n = sc.nextInt();
+       int ans = factorial(n);
+       System.out.println(ans);
+    }
+}
