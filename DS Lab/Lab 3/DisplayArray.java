fbb7b1c02ee0e4d9e9681cd5c34fe407b99a6b61
Diya Santoki <161957088+santokidiya17@users.noreply.github.com>
2024-07-15 22:18:20 +0530

    Lab 3

---
+++ DS Lab/Lab 3/DisplayArray.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class DisplayArray{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array:");
+        int n = sc.nextInt();
+        int a[] = new int[n];
+
+        System.out.println("Enter the value of array:");
+
+        for(int i=0; i<n; i++){
+            a[i] = sc.nextInt();
+        }
+
+        for(int j=0; j<n; j++){
+            System.out.println(a[j]);
+        }
+        sc.close();
+    }
+}
